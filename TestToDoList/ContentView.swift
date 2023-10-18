12da34074b7f8d2a19ea59a243916eb24002662d
Ivan Potyng <nekto_ellez@gmail.com>
2023-10-18 14:54:14 +0700

    Initial Commit

---
+++ TestToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestToDoList
+//
+//  Created by Иван Марин on 18.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestToDoList/TestToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestToDoListApp.swift
+//  TestToDoList
+//
+//  Created by Иван Марин on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
