2585c42b74e65ba638bd9519febc1c6f62a6c9be
Ivan Potyng <nekto_ellez@gmail.com>
2023-10-18 21:46:37 +0700

    feat: Enhance Task List UI and Functionality
    - Added rotation animation to the "plus" button for a more interactive user experience.
    - Implemented swipe-to-delete functionality for each task in the list.
    - Integrated an EditButton in the navigation bar for batch deletions.
    - Fixed the issue where the background was not displaying properly by updating the ZStack order.
    - Added haptic feedback and increased icon size in TaskView.
    
    This commit greatly improves the user interaction and functionality of the Task List.

---
+++ TestToDoList/VIews/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  TestToDoList
+//
+//  Created by Иван Марин on 18.10.2023.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
---
+++ TestToDoList/VIews/TaskView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskView.swift
+//  TestToDoList
+//
+//  Created by Иван Марин on 18.10.2023.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskView()
+}
